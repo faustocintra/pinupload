a461e5d37fa1e6d7c27b14dedada9780a0f70974
alinecintra <alineaparecidacintrasilva@gmail.com>
2019-09-09 22:12:47 -0300

    Primeiro deploy

--- dist/pinupload/main-es5.js
+++ dist/pinupload/main-es5.js
@@ -367,7 +367,7 @@ var environment = {
     production: false,
     authUrl: 'https://api.pinterest.com/oauth/',
     redirectUri: 'http://localhost:4200/login',
-    clientId: '5048713194869147067'
+    clientId: ''
 };
 /*
  * For easier debugging in development mode, you can import the following file
@@ -417,7 +417,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_2__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/goncin/AULA/pinupload/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! C:\Users\1090481713040\Desktop\pinupload\src\main.ts */"./src/main.ts");
 
 
 /***/ })
