01e54d25ae19ccd3462a8ea44c40b864e2041345
Fatec André <topicos.especiais.fatec@gmail.com>
2019-09-20 20:29:43 -0300

    Access Token com async await

--- dist/pinupload/main-es2015.js
+++ dist/pinupload/main-es2015.js
@@ -464,7 +464,6 @@ let PinterestService = class PinterestService {
             // )
             try {
                 const res = yield this.http.post(this.env.authTokenUrl, null, { params }).toPromise();
-                console.log('response', res, res['access_token']);
                 this.accessToken = res['access_token'];
                 console.log('accessToken', this.accessToken);
             }
