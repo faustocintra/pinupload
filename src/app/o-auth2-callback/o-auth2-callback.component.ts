0e5576d86f87a9ff13a9f8ec4503d145eaa0ca9c
EvaldoLucas <evaldosebastiaolucas@gmail.com>
2019-09-21 10:50:16 -0300

    aula200919

--- src/app/o-auth2-callback/o-auth2-callback.component.ts
+++ src/app/o-auth2-callback/o-auth2-callback.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router, ActivatedRoute} from '@Angular/router';
+import { /* Router,*/ ActivatedRoute} from '@angular/router';
 import{PinterestService} from '../services/pinterest.service';
 
 
@@ -11,11 +11,35 @@ import{PinterestService} from '../services/pinterest.service';
 export class OAuth2CallbackComponent implements OnInit {
 
   constructor(
-    private router: Router,
+    //private router: Router,
     private route: ActivatedRoute,
     private pinterest: PinterestService 
   ) { }
 
+
+  ngOnInit() {
+
+    this.route.queryParams.subscribe(
+      params=> {
+
+        if(params.code) {// Se existir o parametro camado 'code'
+
+        console.log('Access code:');
+        console.log(params.code);
+        //Salve o access code para uso posterior
+        this.pinterest.setAccessCode(params.code);
+        //Retorna a pagina inicial
+        //this.router.navigate(['/']);   
+        }
+        else{
+          console.error('ERRO DE ACCESS CODE');
+          //Deu erro no login; retornamos a pagina de login
+          //this.router.navigate(['/']);
+        }
+      }
+    );
+  }
+  /*
   async ngOnInit() {
 
     try{
@@ -41,4 +65,6 @@ export class OAuth2CallbackComponent implements OnInit {
         //this.router.navigate (['/login']);
     }
   }
+
+  */
 }
--- src/app/services/pinterest.service.ts
+++ src/app/services/pinterest.service.ts
@@ -13,28 +13,58 @@ export class PinterestService {
 
   private env = environment;
   private accessCode: string = '';
+  private accessToken : string = '';
 
-  initLogin() 
-{
-  const params = new HttpParams()
-  .set('response_type', 'code')
-  .set('client_id', this.env.clientId)
-  .set('scope', 'read_public,write_public')
-  .set('redirect_uri', this.env.redirectUri);
+    initLogin() 
+  {
+    const params = new HttpParams()
+    .set('response_type', 'code')
+    .set('client_id', this.env.clientId)
+    .set('scope', 'read_public,write_public')
+    .set('redirect_uri', this.env.redirectUri);
 
-//redireciona para o site do pinterest para fazer login
+    //redireciona para o site do pinterest para fazer login
 
-window.location.href = this.env.authUrl + '?' + params.toString();
+    window.location.href = this.env.authUrl + '?' + params.toString();
 
-// asdf
+    // asdf
 
-}
+  }
 
   setAccessCode(accessCode: string) {
 
     this.accessCode = accessCode;
-  }
+    this.getAccessToken();
 
+  }
+  private getAccessToken() {
+    const params = new HttpParams()
+    .set('grant_type', 'authorization_code' )
+    .set('client_id', this.env.clientId)
+    .set ('client_secret', this.env.clientSecret)
+    .set('code', this.accessCode );
+   
+
+    this.http.post(this.env.tokenUri, null, {params: params}).subscribe(
+      res=> {
+        console.log('--TOKEN--');
+        this.accessToken = res['access_token'];
+        console.log(this.accessToken);
+        //this.router.navigate(['/']);
+
+      },
+      error => {
+
+        console.error('ERRO DE TOKEN');
+        console.error(error);
+        //
+      }
+    );
+  }
+}
+
+
+// OBSERVAÇÃO : ESTE METODO NÃO DEU CERTO
 /*async getToken() {
 
     const params = new HttpParams();
@@ -62,4 +92,4 @@ window.location.href = this.env.authUrl + '?' + params.toString();
 
   }*/
 
-}
+
