76739ae90c23119e65430c3db3f502662e815450
ANA CAROLINA SILVA <1090481713001@fatec.local>
2019-09-27 20:19:01 -0300

    AVALIACAO 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { OAuth2CallbackComponent } from './o-auth2-callback/o-auth2-callback.component';
+
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'oauth2/callback', component: OAuth2CallbackComponent }
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/services/pinterest.service.ts
+++ src/app/services/pinterest.service.ts
@@ -13,48 +13,30 @@ export class PinterestService {
 
   private env = environment;
 
-  private accessCode: string = '';
-  private accessToken : string = '';
+  async getToken() {
 
-  initLogin() {
+    const params = new HttpParams();
 
-    const params = new HttpParams()
-      .set('response_type', 'code')
-      .set('client_id', this.env.clientId)
-      .set('scope', 'read_public,write_public')
-      .set('redirect_uri', this.env.redirectUri);
+    params.set('response_type', 'code');
+    params.set('redirect_uri', this.env.redirectUri);
+    params.set('client_id', this.env.clientId);
+    params.set('scope', 'read_public,write_public');
+    params.set('state', 'abc123');
 
-    // Redireciona para o site do Pinterest para fazer login
-    window.location.href = this.env.authUrl + '?' + params.toString();
+    let token;
 
-  }
-
-  setAccessCode(accessCode: string) {
-    this.accessCode = accessCode;
-    this.getAccessToken();
-  }
-
-  private getAccessToken() {
-    const params = new HttpParams()
-      .set('grant_type', 'authorization_code')
-      .set('client_id', this.env.clientId)
-      .set('client_secret', this.env.clientSecret)
-      .set('code', this.accessCode);
-
-      this.http.post(this.env.tokenUri, null, {params: params}).subscribe(
-        res => {
-          console.log('--TOKEN--');
-          this.accessToken = res['access_token'];
-          console.log(this.accessToken);
-          //this.router.navigate(['/']);
-        },
-        error => {
-          console.error('ERRO DE TOKEN');
-          console.error(error);
-          //this.router.navigate(['/login']);
-        }
+    try {
+      const promise = await this.http.get(this.env.authUrl, {params: params});
+      console.log('TOKEN: ' + token);
+      promise.subscribe(
+        ret => token = ret
       );
-    
-  }  
+      return token;
+    }
+    catch(erro) {
+      console.error(erro);
+    }
+
+  }
 
 }
